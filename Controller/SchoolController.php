e918ee0d403fcf9f3300026bcde1afa261d11bc9
Huu Taii <tht.2609@gmail.com>
2025-03-04 11:08:15 +0700

    get school info on index page

--- Controller/SchoolController.php
+++ Controller/SchoolController.php
@@ -20,12 +20,29 @@ class SchoolController extends VSControllerPublic
     public function __construct()
     {
         parent::__construct();
-        // $this->model = VSModel::getInstance()->load($this);
+        $this->model = VSModel::getInstance()->load('Tiimedu');
+        $this->modelSchool = VSModel::getInstance()->load($this->model, 'School/');
+        $this->modelProgram = VSModel::getInstance()->load($this->modelSchool, 'SchoolPrograms');
+        $this->modelLiving = VSModel::getInstance()->load($this->modelSchool, 'SchoolLiving');
+        $this->modelScholarships = VSModel::getInstance()->load($this->modelSchool, 'SchoolScholarships');
+        $this->modelCountry = VSModel::getInstance()->load($this->modelSchool, 'SchoolCountries')->addModel('modelSchool', $this->modelSchool);
+        $this->modelSchool->addModel('modelProgram', $this->modelProgram);
+        $this->currentUser = $this->modelSchool->getLoggined();
+        $this->school = $this->modelSchool->where('user_id', $this->currentUser->getId())->getOne();
+        if(!$this->school) VSRedirect::to('school/pending');
+
+    }
+
+    public function pending()
+    {
+        $this->view->render('Tiimedu/School/pending');
     }
 
     public function index()
     {
-        $this->view->render('Tiimedu/School/index');
+        $this->view->render('Tiimedu/School/index',[
+            'school' => $this->school
+        ]);
     }
 
     public function admission()
